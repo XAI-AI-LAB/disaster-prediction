e86f2795b63d8e2ffb2437432430ad91f1b373d5
Yugi-Seong <77322860+Yugi-Seong@users.noreply.github.com>
2023-06-22 09:44:26 +0900

    Add files via upload

--- ko-sentence-transformers/onnx/test_0607.py
+++ ko-sentence-transformers/onnx/test_0607.py
@@ -41,99 +41,97 @@ if __name__ == "__main__":
     session = ort.InferenceSession(output_fpath)
 
 
-    disaster = '공연장안전사고' 
+    disaster = '항공기사고' 
     # 파일이 들어있는 폴더 경로
-    folder_path = "D:/WorkSpace/ko-sentence-transformers/onnx/{}/".format(disaster)
+    folder_path = "D:/WorkSpace/GNews/{}_csv_folder/사건리스트요약_검색/{}/keyword_list/{}/".format(disaster,disaster,disaster)
 
     # 해당 폴더 내부에 있는 파일 리스트를 가져옵니다.
     folders = os.listdir(folder_path) # 폴더명이 나타남 
     
     # 폴더 내부에 있는 모든 파일 경로를 가져옵니다.
     folder_paths = [os.path.join(folder_path, folder) for folder in folders]   
+    print(folder_paths)
 
-    df = pd.read_csv('D:/WorkSpace/ko-sentence-transformers/onnx/{}/{}_키워드리스트_230614.csv'.format(disaster, disaster))
+##########################
+    df = pd.read_csv('D:/WorkSpace/GNews/{}_csv_folder/사건리스트요약_검색/{}/keyword_list/{}/{}_키워드리스트.csv'.format(disaster, disaster,disaster,disaster))
     print(df)
     for i in range(len(df)): 
         
         queries = df['summarized_article_content'][i]
         
-        for j in range(len(df)):
-            
-            if df['keyword'][j] in folders :
-                queries = df['summarized_article_content'][j]  # 기준이 되는 내용
-                print("============================================queries 출력=====")
-                print(queries)
-                print("==============================================================")
-                # 폴더명과 같은 내용의 폴더 속에 있는 모든 파일을 가져오기 
-                files = glob.glob("D:/WorkSpace/ko-sentence-transformers/onnx/{}/".format(disaster)+df['keyword'][j]+"/"+'*.txt')  
-            
-                df_new = pd.DataFrame(columns=['summarized_article_content', 'queries','score'])
-                # df_new = df_new.append({'summarized_article_content': file_contents, 'queries':queries, 'score': (corpus[idx].strip(), "%.4f" % (cosine[idx]))}, ignore_index=True)
-                
+        KEYWORD = df['keyword'][i]
         
-                # files 속의 파일 내용 읽어서 각각 queries 와 비교하기  
-                for k in range(len(files)):
-                    with open(files[k], 'r', encoding='utf-8') as f :
-                        file_contents = f.read()
-                        
-                        print(file_contents)
-                        
-                        corpus = file_contents
-                                    
-                        tokenized_corpus = tokenizer(corpus, return_tensors="pt", padding=True, truncation=True)
-                        corpus_onnx = {k: v.cpu().detach().numpy() for k, v in tokenized_corpus.items()}
-
-                        tokenized_queries = tokenizer(queries, return_tensors="pt", padding=True, truncation=True)
-                        queries_onnx = {k: v.cpu().detach().numpy() for k, v in tokenized_queries.items()}
-
-                        # run inference
-                        corpus_embeddings, pooled_corpus = session.run(None, corpus_onnx)
-                        corpus_embeddings = mean_pooling(corpus_embeddings, corpus_onnx["attention_mask"])
-
-                        query_embeddings, pooled_queries = session.run(None, queries_onnx)
-                        query_embeddings = mean_pooling(query_embeddings, queries_onnx["attention_mask"])
-
-                        cos_scores = util.pytorch_cos_sim(query_embeddings, corpus_embeddings).cpu()
-
-                        top_k = 1
-
-                        for query, cosine in zip(queries, cos_scores):
-                            print(query)
-                            print(cosine)
-                            top_results = np.argpartition(-cosine, range(top_k))[:top_k]
-                            print(top_results)
-                            print("-----현재 score 출력------")            
-                            print("Query:", query)
-                            
-                            try : 
+        
+        KEYWORD = KEYWORD.replace('"','')
+        KEYWORD = KEYWORD.replace('-','')
+        KEYWORD = KEYWORD.replace(':','')
+        KEYWORD = KEYWORD.replace(';','')
+        KEYWORD = KEYWORD.replace("'",'')
+        KEYWORD = KEYWORD.replace(",",'')
+        KEYWORD = KEYWORD.replace(".",'')
+        KEYWORD = KEYWORD.replace("/",'')
+        
+        if KEYWORD in folders:
+            # 폴더명과 같은 내용의 폴더 속에 있는 모든 파일을 가져오기 
+            files = glob.glob("D:/WorkSpace/GNews/{}_csv_folder/사건리스트요약_검색/{}/keyword_list/{}/".format(disaster,disaster,disaster)+KEYWORD+"/"+'*.txt')          
+            df_new = pd.DataFrame(columns=['summarized_article_content', 'queries','score'])
+                    
+            # files 속의 파일 내용 읽어서 각각 queries 와 비교하기  
+            for k in range(len(files)):
+                with open(files[k], 'r', encoding='utf-8') as f :
+                    file_contents = f.read()
+                    
+                    print(file_contents)
+                    
+                    corpus = file_contents
                                 
-                                for idx in top_results:
-                                    
-                                    print(top_results[idx])
-                                    print(idx)  
-                                    print(corpus[idx].strip(), "(Score: %.4f)" % (cosine[idx]))
-                                    print(corpus[idx].strip(), "%.4f" % (cosine[idx])) # score value
-                            except :
-                                pass             
-                            print("\n")
-                                                
-                        
-                        # 새로운 데이터 프레임 만들어 score 를 저장. 
-                        df_new = df_new.append({'summarized_article_content': file_contents, 'queries':queries, 'score': (cosine)}, ignore_index=True)
-                        df_new['summarized_article_content'][k] = file_contents   
-                        df_new['queries'][k] = queries
-                        # df_new['score'][k] = corpus[idx].strip(), "%.4f" % (cosine[idx])
-                        df_new['score'][k] = cosine
-                    
-                    
-                        try : 
-                            df_new.to_csv('D:/WorkSpace/GNews/{}_csv_folder/사건리스트요약_검색/{}/keyword_list/{}_{}_유사도SCORE.csv'.format(disaster,disaster,today,df['keyword'][j]), encoding='utf-8-sig', index=False)  
-                        
-                        except :
-                            pass
+                    tokenized_corpus = tokenizer(corpus, return_tensors="pt", padding=True, truncation=True)
+                    corpus_onnx = {k: v.cpu().detach().numpy() for k, v in tokenized_corpus.items()}
 
+                    tokenized_queries = tokenizer(queries, return_tensors="pt", padding=True, truncation=True)
+                    queries_onnx = {k: v.cpu().detach().numpy() for k, v in tokenized_queries.items()}
+
+                    # run inference
+                    corpus_embeddings, pooled_corpus = session.run(None, corpus_onnx)
+                    corpus_embeddings = mean_pooling(corpus_embeddings, corpus_onnx["attention_mask"])
+
+                    query_embeddings, pooled_queries = session.run(None, queries_onnx)
+                    query_embeddings = mean_pooling(query_embeddings, queries_onnx["attention_mask"])
+
+                    cos_scores = util.pytorch_cos_sim(query_embeddings, corpus_embeddings).cpu()
+
+                    top_k = 1
+
+                    for query, cosine in zip(queries, cos_scores):
+                        print(query)
+                        print(cosine)
+                        top_results = np.argpartition(-cosine, range(top_k))[:top_k]
+                        print(top_results)
+                        print("-----현재 score 출력------")            
+                        print("Query:", query)
                         
-                        
-                        
-                        
-                
+                        try : 
+                            
+                            for idx in top_results:
+                                
+                                print(top_results[idx])
+                                print(idx)  
+                                print(corpus[idx].strip(), "(Score: %.4f)" % (cosine[idx]))
+                                print(corpus[idx].strip(), "%.4f" % (cosine[idx])) # score value
+                        except :
+                            pass             
+                        print("\n")
+                                            
+                                            
+                    #### 저장 부분 
+                    df_new = df_new.append({'summarized_article_content': file_contents, 'queries':queries, 'score': (cosine)}, ignore_index=True)
+                    df_new['summarized_article_content'][k] = file_contents   
+                    df_new['queries'][k] = queries
+                    df_new['score'][k] = cosine
+                    
+                    
+                    try : 
+                        df_new.to_csv('D:/WorkSpace/GNews/{}_csv_folder/사건리스트요약_검색/{}/keyword_list/{}_{}_유사도SCORE.csv'.format(disaster,disaster,today,KEYWORD), mode='w', encoding='utf-8-sig', index=False)                          
+                    
+                    except :
+                        df_new.to_csv('D:/WorkSpace/GNews/{}_csv_folder/사건리스트요약_검색/{}/keyword_list/{}_{}_유사도SCORE.csv'.format(disaster,disaster,today,KEYWORD), mode='a', encoding='utf-8-sig', index=False)
